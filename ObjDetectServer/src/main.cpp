7991583f9cf93d6c24f9841caf73ca3d55b54f46
Jakub Szewczyk <kubasz51@gmail.com>
2018-10-20 05:50:02 +0100

    Server loads the NN and parses HTTP requests

--- ObjDetectServer/src/main.cpp
+++ ObjDetectServer/src/main.cpp
@@ -1,6 +1,10 @@
 
 #include "objdetserver.h"
 
+#define GPU
+#define OPENCV
+#include <darknet.h>
+
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
@@ -11,6 +15,7 @@
 #include <string>
 #include <cstdint>
 #include <thread>
+#include <mutex>
 
 namespace asio = boost::asio;
 namespace ip = asio::ip;             // from <boost/asio.hpp>
@@ -18,6 +23,9 @@ using tcp = asio::ip::tcp;           // from <boost/asio.hpp>
 namespace http = boost::beast::http; // from <boost/beast/http.hpp>
 
 boost::string_view appSecret;
+
+std::mutex nnMutex;
+network *ann;
 
 template <class Stream, bool isRequest, class Body, class Fields>
 void httpSend(Stream &stream, bool &close, boost::system::error_code &ec, http::message<isRequest, Body, Fields> &&msg)
@@ -29,7 +37,7 @@ void httpSend(Stream &stream, bool &close, boost::system::error_code &ec, http::
 
 void handleConnection(tcp::socket &sock)
 {
-    std::cout << "New connection from " << sock.remote_endpoint().address() << ":" << sock.remote_endpoint().port() << std::endl;
+    std::cerr << "New connection from " << sock.remote_endpoint().address() << ":" << sock.remote_endpoint().port() << std::endl;
 
     bool close = false;
     boost::system::error_code ec;
@@ -62,31 +70,101 @@ void handleConnection(tcp::socket &sock)
         if (rq.method() != http::verb::post)
         {
             httpSend(sock, close, ec, badRequest(http::status::bad_request, "Unknown HTTP-method\n"));
+            std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
             break;
-        }
-        auto fType = rq.find("content-type");
-        if (fType == rq.end()) {
-            httpSend(sock, close, ec, badRequest(http::status::not_implemented, "Unknown request\n"));
-            break;
-        }
-        if (!fType->value().starts_with("multipart/form-data")) {
-            httpSend(sock, close, ec, badRequest(http::status::not_implemented, "Unknown request\n"));
-            break;
-        }
-        for(auto& a : rq) {
-            std::cout << "F <" << a.name_string() << "> := <" << a.value() << ">" << std::endl;
         }
         auto fSecret = rq.find("x-secret");
         if (fSecret == rq.end())
         {
             httpSend(sock, close, ec, badRequest(http::status::unauthorized, "Unauthorized access\n"));
+            std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
             break;
         }
         if (fSecret->value().compare(appSecret) != 0)
         {
             httpSend(sock, close, ec, badRequest(http::status::forbidden, "Forbidden access\n"));
+            std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
             break;
         }
+        auto fType = rq.find("content-type");
+        if (fType == rq.end())
+        {
+            httpSend(sock, close, ec, badRequest(http::status::not_implemented, "Unknown request\n"));
+            std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
+            break;
+        }
+        auto vType = fType->value();
+        boost::string_view boundary;
+        if (!vType.starts_with("multipart/form-data"))
+        {
+            httpSend(sock, close, ec, badRequest(http::status::not_implemented, "Unknown request\n"));
+            std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
+            break;
+        }
+        else
+        {
+            if (vType.find_first_of('=') == vType.npos)
+            {
+                httpSend(sock, close, ec, badRequest(http::status::not_acceptable, "Malformed request\n"));
+                std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
+                break;
+            }
+            boundary = vType;
+            boundary.remove_prefix(vType.find_first_of('=') + 1);
+            if (boundary.length() == 0)
+            {
+                httpSend(sock, close, ec, badRequest(http::status::not_acceptable, "Malformed request\n"));
+                std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
+                break;
+            }
+        }
+        /*for (auto &a : rq)
+        {
+            std::cout << "F <" << a.name_string() << "> := <" << a.value() << ">" << std::endl;
+        }*/
+
+        auto bodyStorage = rq.body();
+        boost::string_view body{bodyStorage};
+        auto postfixPos = [](boost::string_view const &haystack, boost::string_view const &needle) -> size_t {
+            return static_cast<size_t>(std::search(haystack.begin(), haystack.end(), needle.begin(), needle.end()) - haystack.begin());
+        };
+        // --BOUND\n
+        body.remove_prefix(postfixPos(body, boundary) + boundary.size() + 2);
+        // Content-Disposition
+        if (!body.starts_with("Content-Disposition"))
+        {
+            httpSend(sock, close, ec, badRequest(http::status::not_acceptable, "Malformed request\n"));
+            std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
+            break;
+        }
+        body.remove_prefix(body.find_first_of('\n', 0) + 1);
+        // Content-Type
+        if (!body.starts_with("Content-Type"))
+        {
+            httpSend(sock, close, ec, badRequest(http::status::not_acceptable, "Malformed request\n"));
+            std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
+            break;
+        }
+        body.remove_prefix(body.find_first_of('\n', 0) + 1);
+        // Content-Length
+        if (!body.starts_with("Content-Length"))
+        {
+            httpSend(sock, close, ec, badRequest(http::status::not_acceptable, "Malformed request\n"));
+            std::cerr << "HTTP Error at line " << __LINE__ << std::endl;
+            break;
+        }
+        body.remove_prefix(body.find_first_of('\n', 0) + 1);
+        while (body.size() && (body[0] == '\n' || body[0] == '\r'))
+        {
+            body.remove_prefix(1);
+        }
+        // terminating boundary
+        size_t it = postfixPos(body, boundary);
+        while (body.at(it) != '\n')
+            it--;
+        body = body.substr(0, it);
+        // body is now the image
+
         std::string json = "{}\n";
         http::response<http::string_body> res{std::piecewise_construct, std::make_tuple(json), std::make_tuple(http::status::ok, rq.version())};
         res.set(http::field::content_type, "application/json");
@@ -102,9 +180,9 @@ int main(int argc, char **argv)
 {
     try
     {
-        if (argc != 6)
+        if (argc != 7)
         {
-            std::cerr << "Usage: " << argv[0] << " <bind address> <bind port> <secret> <cfg> <weights>" << std::endl;
+            std::cerr << "Usage: " << argv[0] << " <bind address> <bind port> <secret> <data> <cfg> <weights>" << std::endl;
             return EXIT_FAILURE;
         }
 
@@ -112,6 +190,28 @@ int main(int argc, char **argv)
         uint16_t bindPort = static_cast<uint16_t>(std::strtol(argv[2], nullptr, 10));
         appSecret = argv[3];
         //std::cerr << argv[2] << " == " << bindPort << std::endl;
+
+        // Initialize NN
+        {
+            std::cerr << "Loading NN..." << std::endl;
+            cuda_set_device(0);
+            float thresh = 0.25f;
+            float hier_thresh = 0.4f;
+            char *datafile = argv[4];
+            char *cfgfile = argv[5];
+            char *weights = argv[6];
+
+            list *options = read_data_cfg(datafile);
+            int classes = option_find_int(options, const_cast<char *>("classes"), 20);
+            char *name_list = option_find_str(options, const_cast<char *>("names"),
+                                              const_cast<char *>("darknet/data/names.list"));
+            char **names = get_labels(name_list);
+
+            network *net = load_network(cfgfile, weights, 0);
+            set_batch_network(net, 1);
+            ann = net;
+            std::cerr << "Loaded NN" << std::endl;
+        }
 
         asio::io_context ioc{1};
 
