e7e2373b40670ce21e194f8852c883341c575d71
rphi <8237136+rphi@users.noreply.github.com>
2018-10-20 14:35:19 +0200

    Now reads and renders classes from ODS

--- JobCam/app/src/main/java/com/hackupc/uoe/jobcam/CameraActivity.kt
+++ JobCam/app/src/main/java/com/hackupc/uoe/jobcam/CameraActivity.kt
@@ -1,27 +1,27 @@
 package com.hackupc.uoe.jobcam
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.graphics.*
 import android.os.Bundle
-import android.view.View
-import com.wonderkiln.camerakit.*
-import okhttp3.*
-import java.io.IOException
-
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-
-import java.io.ByteArrayOutputStream
-import android.opengl.ETC1.getHeight
-import android.opengl.ETC1.getWidth
 import android.os.Handler
 import android.os.Message
+import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
-
+import com.beust.klaxon.Klaxon
 import com.hackupc.uoe.jobcam.Components.Boundary
-import com.wonderkiln.camerakit.CameraKit.Constants.*
+import com.hackupc.uoe.jobcam.Components.MLresponse
+import com.hackupc.uoe.jobcam.Components.MLresults
+import com.wonderkiln.camerakit.CameraKit
+import com.wonderkiln.camerakit.CameraKit.Constants.METHOD_STILL
+import com.wonderkiln.camerakit.CameraView
+import okhttp3.*
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.lang.Math.*
 import kotlin.concurrent.thread
+import kotlin.math.sqrt
 
 
 class CameraActivity : Activity() {
@@ -29,9 +29,15 @@ class CameraActivity : Activity() {
     private var cameraView: CameraView? = null
     private var imageView: ImageView? = null
 
+    private var boundaries: Array<Boundary>? = null
+
     // This is the activity main thread Handler.
     private var updateUIHandler: Handler? = null
 
+    var drawing: Bitmap? = null
+    var canvas: Canvas? = null
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -40,14 +46,36 @@ class CameraActivity : Activity() {
         createUpdateUiHandler()
 
         cameraView = findViewById(R.id.camera)
-        cameraView!!.setMethod(METHOD_STILL);
+        cameraView!!.setMethod(METHOD_STILL)
 
         val button = findViewById<View>(R.id.floatingActionButton3)
         imageView = findViewById(R.id.imageView)
+        imageView!!.addOnLayoutChangeListener { view, left, top, right, bottom, oldLeft, oldRight, oldTop, oldBottom ->
+            val viewWidth = right - left
+            if (viewWidth > 0) {
+                thread {
+                    drawing = Bitmap.createBitmap(imageView!!.width, imageView!!.height, Bitmap.Config.ARGB_8888)
+                    canvas = Canvas(drawing)
+
+                }
+            }
+        }
+
+        imageView!!.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event?.action) {
+                    MotionEvent.ACTION_DOWN -> overlayClick(event)//Do Something
+                }
+
+                return v?.onTouchEvent(event) ?: true
+            }
+        })
 
         button.setOnClickListener {
             thread { capture() }
         }
+
+
     }
 
     override fun onStart() {
@@ -88,7 +116,7 @@ class CameraActivity : Activity() {
     var client = OkHttpClient()
 
     @Throws(IOException::class)
-    fun post(url: String, byteArray: ByteArray): String {
+    fun post(url: String, byteArray: ByteArray) {
         var body = MultipartBody.Builder()
                 .setType(MultipartBody.FORM)
                 .addFormDataPart("image", "", RequestBody.create(MediaType.parse("image/*"), byteArray))
@@ -99,24 +127,23 @@ class CameraActivity : Activity() {
                 .addHeader("X-secret", "hi")
                 .build()
         val response = client.newCall(request).execute()
-        return response.body()!!.string()
+        receive(json = response.body()!!.string())
     }
 
     fun capture() {
         cameraView?.captureImage {
             val stream = ByteArrayOutputStream()
-            it.bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
+            it.bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream)
             val image = stream.toByteArray()
-            //post("http://localhost:8000/testpost", image)
-            receive("test")
+            post("http://10.42.0.1:12345/", image)
+            //receive("test")
         }
     }
 
     fun receive(json: String) {
-        var drawing = Bitmap.createBitmap(imageView!!.width, imageView!!.height, Bitmap.Config.ARGB_8888)
-        var canvas = Canvas(drawing)
+
         val boundaries = parseBoundaries(json)
-        drawBoundaries(boundaries, canvas)
+        drawBoundaries(boundaries, canvas!!)
 
         // Build message object.
         val message = Message()
@@ -127,7 +154,12 @@ class CameraActivity : Activity() {
         updateUIHandler!!.sendMessage(message)
     }
 
-    fun drawBoundaries(boundaries: Array<Boundary>, canvas: Canvas){
+    fun drawBoundaries(boundaries: Array<Boundary>, canvas: Canvas?){
+        if (canvas == null) {return}
+
+        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
+
+
         val paint = Paint()
         paint.style = Paint.Style.STROKE
         paint.color = Color.RED
@@ -137,12 +169,56 @@ class CameraActivity : Activity() {
             canvas.drawCircle(it.centre_x, it.centre_y, it.radius, paint)
             canvas.drawText(it.label, it.centre_x, it.centre_y, paint)
         }
+        this.boundaries = boundaries
     }
 
     fun parseBoundaries(json: String) : Array<Boundary> {
-        var array = arrayOf(
-            Boundary("test", 400.4f, 600.0f, 200.0f)
-        )
-        return array
+
+        val resJSON = Klaxon().parse<MLresponse>(json)!!.results
+
+        val boundaryList = arrayListOf<Boundary>()
+
+        resJSON!!.forEach {
+            boundaryList.add(Boundary(label = it.classes[0],
+                    centre_x = (it.bbox[0] * imageView!!.width).toFloat(),
+                    centre_y = (it.bbox[1] * imageView!!.height).toFloat(),
+                    radius = (min(it.bbox[2], it.bbox[3]) * imageView!!.width) .toFloat()
+            ))
+        }
+        return boundaryList.toTypedArray()
+    }
+
+    fun overlayClick(event: MotionEvent?) {
+        if (boundaries == null) { return } else if (boundaries!!.isEmpty()) { return }
+
+        val x = event!!.x
+        val y = event!!.y
+        val viewCoords = IntArray(2)
+        imageView!!.getLocationOnScreen(viewCoords)
+        val imageX = x - viewCoords[0] // viewCoords[0] is the X coordinate
+        val imageY = y - viewCoords[1] // viewCoords[1] is the y coordinate
+
+        var dist: Double? = null
+        var tapped: String? = null
+
+        boundaries!!.forEach {
+            val distFromCentre = sqrt(pow(imageX.toDouble() - it.centre_x, 2.0) + pow(imageY.toDouble() - it.centre_y, 2.0))
+
+            if ( dist == null ) {
+                dist = distFromCentre
+            }
+            if ( distFromCentre < it.radius.toDouble() && distFromCentre <= dist!!) {
+                tapped = it.label
+            }
+        }
+
+        if (tapped != null) {
+            // we've found a tap on an object
+            tappedOn(tapped!!)
+        }
+    }
+
+    fun tappedOn(tapped: String){
+        print(tapped)
     }
 }
--- JobCam/app/src/main/java/com/hackupc/uoe/jobcam/Components/Boundary.kt
+++ JobCam/app/src/main/java/com/hackupc/uoe/jobcam/Components/Boundary.kt
@@ -4,3 +4,8 @@ data class Boundary (val label: String,
                      val centre_x: Float,
                      val centre_y: Float,
                      val radius: Float)
+
+data class MLresponse (val results: Array<MLresults>)
+
+data class MLresults (val bbox: DoubleArray,
+                      val classes: Array<String>)
