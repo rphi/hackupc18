c4d8d9d644d372a069d6088a8cfb87d83beb78a1
Rob P <8237136+rphi@users.noreply.github.com>
2018-10-20 03:40:24 +0100

    Add test capture button and http stuff (#2)

--- JobCam/app/src/main/java/com/hackupc/uoe/jobcam/CameraActivity.kt
+++ JobCam/app/src/main/java/com/hackupc/uoe/jobcam/CameraActivity.kt
@@ -2,7 +2,14 @@ package com.hackupc.uoe.jobcam
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import com.wonderkiln.camerakit.*
+import okhttp3.*
+import java.io.IOException
+
+import android.graphics.Bitmap
+
+import java.io.ByteArrayOutputStream
 
 class CameraActivity : Activity() {
 
@@ -12,6 +19,12 @@ class CameraActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         cameraView = findViewById(R.id.camera)
+
+        val button = findViewById<View>(R.id.floatingActionButton3)
+
+        button.setOnClickListener {
+            capture()
+        }
     }
 
     override fun onStart() {
@@ -28,5 +41,32 @@ class CameraActivity : Activity() {
     override fun onStop() {
         cameraView?.stop()
         super.onStop()
+    }
+
+    val JSON = MediaType.parse("application/json; charset=utf-8")
+    var client = OkHttpClient()
+
+    @Throws(IOException::class)
+    fun post(url: String, byteArray: ByteArray): String {
+        var body = MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("image", "", RequestBody.create(MediaType.parse("image/*"), byteArray))
+                .build()
+        val request = Request.Builder()
+                .url(url)
+                .post(body)
+                .addHeader("X-secret", "hi")
+                .build()
+        val response = client.newCall(request).execute()
+        return response.body()!!.string()
+    }
+
+    fun capture() {
+        cameraView?.captureImage {
+            val stream = ByteArrayOutputStream()
+            it.bitmap.compress(Bitmap.CompressFormat.PNG, 90, stream)
+            val image = stream.toByteArray()
+            post("http://localhost:8000/testpost", image)
+        }
     }
 }
--- JobCam/app/src/main/java/com/hackupc/uoe/jobcam/MainActivity.kt
+++ JobCam/app/src/main/java/com/hackupc/uoe/jobcam/MainActivity.kt
@@ -21,7 +21,4 @@ class MainActivity : Activity() {
             }
         })
     }
-
-
-
 }
