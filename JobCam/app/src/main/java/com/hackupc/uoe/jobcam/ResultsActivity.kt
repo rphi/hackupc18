67a50dece64037708fecddd8301fe3084ffe6ab3
https://bnelo12@github.com <s1623919@sms.ed.ac.uk>
2018-10-20 06:14:00 +0200

    Added infocom API access

--- JobCam/app/src/main/java/com/hackupc/uoe/jobcam/ResultsActivity.kt
+++ JobCam/app/src/main/java/com/hackupc/uoe/jobcam/ResultsActivity.kt
@@ -2,23 +2,32 @@ package com.hackupc.uoe.jobcam
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 
 import okhttp3.*
 import java.io.IOException
+import kotlin.concurrent.thread
 
 class ResultsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val url = "api.infojobs.net"
-        val endpoint = "/api/1/offer"
+        setContentView(R.layout.result_layout)
+        thread() {
+            // Do Search Here
+        }
     }
 
-    @Throws(IOException::class)
-    fun getSearchResults(searchWord: String) {
-        val request = Request.Builder()
-                .url("https://api.infojobs.net/api/1/offer")
-                .addHeader("","")
+    private var client = OkHttpClient()
 
+    @Throws(IOException::class)
+    fun getSearchResults(searchWord: String): String? {
+        val request = Request.Builder()
+                .url("https://api.infojobs.net/api/1/offer?q=" + searchWord)
+                .addHeader("Authorization", "basic " + resources.getString(R.string.infojob_api_key))
+                .build()
+
+        val response = client.newCall(request).execute()
+        return response.body()!!.string()
 
     }
 }
